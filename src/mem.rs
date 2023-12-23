ed39d5c7d7be01b9eb2c71ef2a6ffa0bcb616492
ciciplusplus <ciciplusplus@gmail.com>
2023-12-24 00:08:23 +0100

    bypass null check 🙈
    Change-Id: If8ba7fcff92f1b3226e292b23b087702b3589724

--- src/mem.rs
+++ src/mem.rs
@@ -372,7 +372,8 @@ impl Mem {
     /// [Self::ptr_at] for that).
     pub fn bytes_at<const MUT: bool>(&self, ptr: Ptr<u8, MUT>, count: GuestUSize) -> &[u8] {
         if ptr.to_bits() < self.null_segment_size {
-            Self::null_check_fail(ptr.to_bits(), count)
+            //Self::null_check_fail(ptr.to_bits(), count)
+            return &[]
         }
         &self.bytes()[ptr.to_bits() as usize..][..count as usize]
     }
