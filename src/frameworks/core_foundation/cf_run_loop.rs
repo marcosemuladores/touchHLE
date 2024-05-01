62419afeed6dbda7919fa8ad5e6220797be9cd7b
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-05-01 18:07:39 +0200

    Update cf_run_loop.rs

--- src/frameworks/core_foundation/cf_run_loop.rs
+++ src/frameworks/core_foundation/cf_run_loop.rs
@@ -8,8 +8,13 @@
 //! This is not even toll-free bridged to `NSRunLoop` in Apple's implementation,
 //! but here it is the same type.
 
+use crate::abi::GuestFunction;
 use crate::dyld::{export_c_func, ConstantExports, FunctionExports, HostConstant};
-use crate::objc::msg_class;
+use crate::frameworks::core_foundation::cf_allocator::CFAllocatorRef;
+use crate::frameworks::core_foundation::time::{CFAbsoluteTime, CFTimeInterval};
+use crate::frameworks::core_foundation::CFIndex;
+use crate::mem::MutVoidPtr;
+use crate::objc::{id, msg, msg_class, nil, Class};
 use crate::{Environment, msg};
 use crate::frameworks::core_foundation::cf_string::CFStringRef;
 use crate::frameworks::core_foundation::{CFIndex, CFTypeRef};
@@ -19,6 +24,9 @@ use crate::mem::{ConstVoidPtr, Ptr};
 
 pub type CFRunLoopRef = super::CFTypeRef;
 pub type CFRunLoopMode = super::cf_string::CFStringRef;
+
+pub type CFRunLoopTimerRef = super::CFTypeRef;
+pub type CFOptionFlags = u32;
 
 fn CFRunLoopGetCurrent(env: &mut Environment) -> CFRunLoopRef {
     msg_class![env; NSRunLoop currentRunLoop]
@@ -35,6 +43,60 @@ fn CFRunLoopRunInMode(
     // () = msg![env; loop_ run];
     // 0
     1
+}
+
+// CFRunLoopTimerRef CFRunLoopTimerCreate(
+// CFAllocatorRef allocator, CFAbsoluteTime fireDate, CFTimeInterval interval,
+// CFOptionFlags flags, CFIndex order, CFRunLoopTimerCallBack callout,
+// CFRunLoopTimerContext *context)
+
+// typedef void (*CFRunLoopTimerCallBack)(CFRunLoopTimerRef timer, void *info)
+fn CFRunLoopTimerCreate(
+    env: &mut Environment,
+    _allocator: CFAllocatorRef,
+    _fire_date: CFAbsoluteTime,
+    interval: CFTimeInterval,
+    flags: CFOptionFlags,
+    order: CFIndex,
+    callout: GuestFunction,
+    context: MutVoidPtr,
+) -> CFRunLoopTimerRef {
+    assert_eq!(flags, 0);
+    assert_eq!(order, 0);
+    // assert!(context.is_null());
+
+    let fake_target: id = msg_class![env; FakeCFTimerTarget alloc];
+    let fake_target: id = msg![env; fake_target initWithCallout:callout context:context];
+
+    let selector = env.objc.lookup_selector("timerFireMethod:").unwrap();
+
+    let repeats = interval > 0.0;
+    msg_class![env; NSTimer timerWithTimeInterval:interval
+                                           target:fake_target
+                                         selector:selector
+                                         userInfo:nil
+                                          repeats:repeats]
+}
+
+fn CFRunLoopAddTimer(
+    env: &mut Environment,
+    rl: CFRunLoopRef,
+    timer: CFRunLoopTimerRef,
+    mode: CFRunLoopMode,
+) {
+    let rl_class: Class = msg![env; rl class];
+    assert_eq!(
+        rl_class,
+        env.objc.get_known_class("NSRunLoop", &mut env.mem)
+    );
+
+    let timer_class: Class = msg![env; timer class];
+    assert_eq!(
+        timer_class,
+        env.objc.get_known_class("NSTimer", &mut env.mem)
+    );
+
+    () = msg![env; rl addTimer:timer forMode:mode];
 }
 
 fn CFPreferencesCopyAppValue(env: &mut Environment, key: CFStringRef, appID: CFStringRef) -> CFTypeRef {
@@ -83,6 +145,8 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(CFRunLoopGetCurrent()),
     export_c_func!(CFRunLoopGetMain()),
     export_c_func!(CFRunLoopRunInMode(_, _, _)),
+    export_c_func!(CFRunLoopTimerCreate(_, _, _, _, _, _, _)),
+    export_c_func!(CFRunLoopAddTimer(_, _, _)),
     export_c_func!(CFPreferencesCopyAppValue(_, _)),
     export_c_func!(CFPreferencesSetAppValue(_, _, _)),
     export_c_func!(CFPreferencesAppSynchronize(_)),
