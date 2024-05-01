a521b5187fde9cc068caff1987a878d429039e46
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-05-01 16:05:32 +0200

    Update ui_image_picker_controller.rs

--- src/frameworks/uikit/ui_image_picker_controller.rs
+++ src/frameworks/uikit/ui_image_picker_controller.rs
@@ -7,6 +7,7 @@
 
 use crate::frameworks::foundation::NSInteger;
 use crate::objc::{objc_classes, ClassExports};
+use crate::objc::{id, SEL, nil};
 
 type UIImagePickerControllerSourceType = NSInteger;
 
@@ -24,6 +25,15 @@ pub const CLASSES: ClassExports = objc_classes! {
     false
 }
 
+@end
+
+@implementation UIBarButtonItem: NSObject
+- (id)initWithBarButtonSystemItem:(NSInteger)item target:(id)target action:(SEL)action {
+    nil
+}
+- (id)initWithCustomView:(id)view {
+    nil
+}
 @end
 
 };
