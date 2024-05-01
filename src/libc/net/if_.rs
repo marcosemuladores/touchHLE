b0469aa2cbcb4d76ea750f54a4a1a5ffabf63c51
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-05-01 14:07:35 +0200

    Update if_.rs

--- src/libc/net/if_.rs
+++ src/libc/net/if_.rs
@@ -7,8 +7,11 @@
 
 use crate::dyld::FunctionExports;
 use crate::export_c_func;
-use crate::mem::{ConstPtr, Ptr};
+use crate::mem::{ConstPtr, MutVoidPtr, Ptr};
 use crate::Environment;
+use crate::frameworks::core_foundation::cf_allocator::CFAllocatorRef;
+use crate::frameworks::core_foundation::CFTypeRef;
+use crate::objc::nil;
 
 // TODO: struct definition
 #[allow(non_camel_case_types)]
@@ -19,4 +22,22 @@ fn if_nameindex(_env: &mut Environment) -> ConstPtr<if_nameindex> {
     Ptr::null()
 }
 
-pub const FUNCTIONS: FunctionExports = &[export_c_func!(if_nameindex())];
+fn SCNetworkReachabilityCreateWithAddress(_env: &mut Environment, allocator: CFAllocatorRef, socket: MutVoidPtr) -> CFTypeRef {
+    nil
+}
+
+fn SCNetworkReachabilityGetFlags(_env: &mut Environment, target: CFTypeRef, flags: MutVoidPtr) -> bool {
+    false
+}
+
+fn gethostbyname(_env: &mut Environment, name: ConstPtr<u8>) -> MutVoidPtr {
+    Ptr::null()
+}
+
+pub const FUNCTIONS: FunctionExports = &[
+    export_c_func!(if_nameindex()),
+    export_c_func!(SCNetworkReachabilityCreateWithAddress(_, _)),
+    export_c_func!(SCNetworkReachabilityGetFlags(_, _)),
+    export_c_func!(SCNetworkReachabilityGetFlags(_, _)),
+    export_c_func!(gethostbyname(_)),
+];
