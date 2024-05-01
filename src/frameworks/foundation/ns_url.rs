12dc1cea44eb31142ae3e19595d7d47f73b722f1
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-05-01 22:31:53 +0200

    Update ns_url.rs

--- src/frameworks/foundation/ns_url.rs
+++ src/frameworks/foundation/ns_url.rs
@@ -6,7 +6,7 @@
 //! `NSURL`.
 
 use super::ns_string::{from_rust_string, to_rust_string, NSUTF8StringEncoding};
-use super::NSUInteger;
+use super::{NSUInteger, NSTimeInterval};
 use crate::fs::{GuestPath, GuestPathBuf};
 use crate::mem::MutPtr;
 use crate::objc::{
@@ -164,6 +164,25 @@ pub const CLASSES: ClassExports = objc_classes! {
 
 @end
 
+@implementation NSURLRequest: NSObject
++ (id)requestWithURL:(id)URL // NSURL *
+                   cachePolicy:(NSUInteger)cachePolicy
+               timeoutInterval:(NSTimeInterval)timeoutInterval {
+    nil
+}
+@end
+
+@implementation NSURLConnection: NSObject
++ (id)sendSynchronousRequest:(id)request
+           returningResponse:(MutPtr<id>)response
+                       error:(MutPtr<id>)error {
+    nil
+}
+- (id)initWithRequest:(id)req delegate:(id)deleg {
+    nil
+}
+@end
+    
 };
 
 /// Shortcut for host code, provides a view of a URL as a path.
