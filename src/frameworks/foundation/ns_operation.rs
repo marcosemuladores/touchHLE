bd4ebca8178e3cb7195ba84143d11c313fbf1b3b
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-05-02 05:56:05 +0200

    Create ns_operation.rs

---
+++ src/frameworks/foundation/ns_operation.rs
@@ -0,0 +1,13 @@
+use crate::objc::{
+    objc_classes, ClassExports,
+};
+
+pub const CLASSES: ClassExports = objc_classes! {
+
+(env, this, _cmd);
+
+@implementation NSOperation: NSObject
+
+@end
+
+};
