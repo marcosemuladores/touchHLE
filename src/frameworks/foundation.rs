0b5e5c9e6824c874c15a19f9756557c385982b03
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-05-02 09:08:08 +0200

    Update foundation.rs

--- src/frameworks/foundation.rs
+++ src/frameworks/foundation.rs
@@ -46,7 +46,6 @@ pub mod ns_process_info;
 pub mod ns_property_list_serialization;
 pub mod ns_run_loop;
 pub mod ns_set;
-pub mod ns_sort_descriptor;
 pub mod ns_string;
 pub mod ns_thread;
 pub mod ns_time_zone;
