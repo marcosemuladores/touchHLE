9862fdaef9b92070dbf2c81f2af25b2125913c27
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-05-02 05:58:57 +0200

    Update ns_property_list_serialization.rs

--- src/frameworks/foundation/ns_property_list_serialization.rs
+++ src/frameworks/foundation/ns_property_list_serialization.rs
@@ -1,12 +1,17 @@
 //! `NSPropertyListSerialization`.
 
-use super::{ns_array, ns_dictionary, ns_string, NSUInteger};
+use super::{ns_array, ns_data, ns_dictionary, ns_string, NSUInteger};
 use crate::fs::GuestPath;
 use crate::mem::MutPtr;
-use crate::objc::{id, msg, msg_class, nil, release};
 use crate::Environment;
-use plist::Value;
+use crate::objc::{id, msg, msg_class, nil, release, objc_classes, Class, ClassExports};
 use std::io::Cursor;
+
+use plist::Value;
+
+use crate::frameworks::foundation::ns_dictionary::{DictionaryHostObject, dict_from_keys_and_objects};
+use crate::frameworks::foundation::ns_string::to_rust_string;
+use crate::frameworks::foundation::ns_value::NSNumberHostObject;
 
 // TODO: Implement reading of property lists other than Info.plist.
 // [NSDictionary contentsOfFile:] and [NSArray contentsOfFile:] in particular.
@@ -113,4 +118,75 @@ fn deserialize_plist(env: &mut Environment, value: &Value) -> id {
             unreachable!() // enum is marked inexhaustive, but shouldn't be
         }
     }
+}
+
+pub const CLASSES: ClassExports = objc_classes! {
+
+(env, this, _cmd);
+
+@implementation NSPropertyListSerialization: NSObject
+
++ (id)dataFromPropertyList:(id)plist
+                    format:(i32)format
+                errorDescription:(id)errorString { // NSString**
+    // 200 => NSPropertyListBinaryFormat_v1_0 = kCFPropertyListBinaryFormat_v1_0
+    assert_eq!(format, 200);
+    log_dbg!("dataFromPropertyList format {}", format);
+    let value = build_plist_value_rec(env, plist);
+    let mut buf = Vec::new();
+    value.to_writer_binary(&mut buf).unwrap();
+    let len: u32 = buf.len().try_into().unwrap();
+    log_dbg!("dataFromPropertyList buf len {}", len);
+    let ptr = env.mem.alloc_and_write_cstr(&buf[..]);
+    msg_class![env; NSData dataWithBytes:ptr length:len]
+}
+
++ (id)propertyListFromData:(id)data // NSData*
+          mutabilityOption:(NSUInteger)opt
+                    format:(MutPtr<i32>)format
+          errorDescription:(id)errorString { // NSString**
+    // assert!(format.is_null());
+    let slice = ns_data::to_rust_slice(env, data);
+    let plist = Value::from_reader(Cursor::new(slice)).unwrap();
+    let plist = plist.into_dictionary().unwrap();
+    // TODO: parse plist to objects
+    dict_from_keys_and_objects(env, &[])
+}
+
+@end
+
+};
+
+fn build_plist_value_rec(env: &mut Environment, plist: id) -> Value {
+    let class: Class = msg![env; plist class];
+
+    // TODO: check subclass instead of exact match
+    return if class == env.objc.get_known_class("NSMutableDictionary", &mut env.mem) {
+        let mut dict = plist::dictionary::Dictionary::new();
+        let dict_host_obj: DictionaryHostObject = std::mem::take(env.objc.borrow_mut(plist));
+        let mut iter = dict_host_obj.iter_keys();
+        while let Some(key) = iter.next() {
+            let key_class: Class = msg![env; key class];
+
+            // only string keys are supported
+            let string_class = env.objc.get_known_class("_touchHLE_NSString", &mut env.mem);
+            assert!(env.objc.class_is_subclass_of(key_class, string_class));
+
+            let key_string = to_rust_string(env, key);
+            let val = dict_host_obj.lookup(env, key);
+            let val_plist = build_plist_value_rec(env, val);
+            dict.insert(String::from(key_string), val_plist);
+        }
+        Value::Dictionary(dict)
+    } else if class == env.objc.get_known_class("NSNumber", &mut env.mem) {
+        let num = env.objc.borrow::<NSNumberHostObject>(plist);
+        match num {
+            NSNumberHostObject::Bool(b) => Value::Boolean(*b),
+            NSNumberHostObject::Int(i) => Value::from(*i),
+            NSNumberHostObject::Float(f) => Value::from(*f),
+            _ => todo!()
+        }
+    } else {
+        unimplemented!()
+    };
 }
