3c4920ff7fc02ca87e722dfcb98954d4cbf1ac32
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-05-03 13:05:52 +0200

    Update classes.rs

--- src/objc/classes.rs
+++ src/objc/classes.rs
@@ -431,6 +431,7 @@ fn substitute_classes(
     // to skip this. This isn't "ad blocking" because ads no longer work on real
     // devices anyway :)
     if !(name.starts_with("AdMob") || name.starts_with("AltAds") || name.starts_with("Mobclix")) {
+        || name.starts_with("FlurryAPI") || name.starts_with("PreferencesController") || name.starts_with("WebViewController")) {
         return None;
     }
 
