1c1cd4a7dfef902a68dcb5df47ffd95c4436678b
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-05-04 15:32:39 +0200

    Update gles1_native.rs

--- src/gles/gles1_native.rs
+++ src/gles/gles1_native.rs
@@ -700,10 +700,6 @@ impl GLES for GLES1Native {
         gles11::GetFramebufferAttachmentParameterivOES(target, attachment, pname, params)
     }
 
-    unsafe fn PolygonOffset(&mut self, factor: GLfloat, units: GLfloat) {
-        gles11::PolygonOffset(factor, units)    
-    }
-
     unsafe fn IsTexture(&mut self, texture: GLuint) -> GLboolean {
         gles11::IsTexture(texture)
     }
