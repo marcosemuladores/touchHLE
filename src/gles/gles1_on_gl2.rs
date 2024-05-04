d7c4eceaf3e89edae8ca383d2a09aa20d243ac9d
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-05-04 15:33:47 +0200

    Update gles1_on_gl2.rs

--- src/gles/gles1_on_gl2.rs
+++ src/gles/gles1_on_gl2.rs
@@ -1787,10 +1787,6 @@ impl GLES for GLES1OnGL2 {
     unsafe fn GenerateMipmapOES(&mut self, target: GLenum) {
         gl21::GenerateMipmapEXT(target)
     }
-
-    unsafe fn PolygonOffset(&mut self, factor: GLfloat, units: GLfloat) {
-        gl21::PolygonOffset(factor, units)
-    }    
     unsafe fn IsTexture(&mut self, texture: GLuint) -> GLboolean {
         gl21::IsTexture(texture)
     }
