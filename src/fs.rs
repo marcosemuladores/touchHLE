698eb95313eaa103cc416b215721ad2b50b2df37
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-05-04 08:45:48 +0200

    Update fs.rs

--- src/fs.rs
+++ src/fs.rs
@@ -467,8 +467,7 @@ impl Fs {
 
         let bundle_guest_path = home_directory.join(&bundle_dir_name);
 
-        let mut documents_host_path = if !read_only_mode {
-        let mut library_host_path = None;
+        let documents_host_path = if !read_only_mode {
             let path = paths::user_data_base_path()
                 .join(paths::SANDBOX_DIR)
                 .join(bundle_id)
@@ -478,22 +477,6 @@ impl Fs {
                     "Could not create documents directory for app at {:?}: {:?}",
                     path, e
                 );
-            }
-            let libs_host_path = base_path.join("Library");
-            if let Err(e) = std::fs::create_dir_all(&libs_host_path) {
-                panic!(
-                    "Could not create library directory for app at {:?}: {:?}",
-                    libs_host_path, e
-                );
-            }
-            let app_support_host_path = libs_host_path.join("Application Support");
-            if let Err(e) = std::fs::create_dir_all(&app_support_host_path) {
-                panic!(
-                    "Could not create application support directory for app at {:?}: {:?}",
-                    app_support_host_path, e
-                );
-            }
-            library_host_path = Some(libs_host_path)
             }
             Some(path)
         } else {
@@ -523,12 +506,6 @@ impl Fs {
             app_dir_children.insert(
                 "Documents".to_string(),
                 FsNode::from_host_dir(&documents_host_path, /* writeable: */ true),
-            );
-        }
-        if let Some(library_host_path) = library_host_path {
-            app_dir_children.insert(
-                "Library".to_string(),
-                FsNode::from_host_dir(&library_host_path, /* writeable: */ true),
             );
         }
 
