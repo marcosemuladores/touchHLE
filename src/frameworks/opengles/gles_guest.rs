fa73d3e7f105527b95217addee8da58f596a6c54
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-05-04 15:36:58 +0200

    Update gles_guest.rs

--- src/frameworks/opengles/gles_guest.rs
+++ src/frameworks/opengles/gles_guest.rs
@@ -1113,15 +1113,6 @@ fn glGetFramebufferAttachmentParameterivOES(
         unsafe { gles.GetFramebufferAttachmentParameterivOES(target, attachment, pname, params) }
     })
 }
-fn glPolygonOffset(
-    env: &mut Environment,
-    factor: GLfloat,
-    units: GLfloat
-) {
-    with_ctx_and_mem(env, |gles, mem| {
-        unsafe { gles.PolygonOffset(factor, units) }
-    })
-}
 fn glCheckFramebufferStatusOES(env: &mut Environment, target: GLenum) -> GLenum {
     with_ctx_and_mem(env, |gles, _mem| unsafe {
         gles.CheckFramebufferStatusOES(target)
@@ -1394,7 +1385,6 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(glFramebufferTexture2DOES(_, _, _, _, _)),
     export_c_func!(glGetRenderbufferParameterivOES(_, _, _)), 
     export_c_func!(glGetFramebufferAttachmentParameterivOES(_, _, _, _)),
-    export_c_func!(glPolygonOffset(_, _)),
     export_c_func!(glCheckFramebufferStatusOES(_)),
     export_c_func!(glDeleteFramebuffersOES(_, _)),
     export_c_func!(glDeleteRenderbuffersOES(_, _)),
