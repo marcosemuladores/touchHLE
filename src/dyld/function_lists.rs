7d5f81c483015136a1ff91f2bc284acd186571b9
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-05-04 19:49:36 +0200

    Update function_lists.rs

--- src/dyld/function_lists.rs
+++ src/dyld/function_lists.rs
@@ -21,7 +21,6 @@ pub const FUNCTION_LISTS: &[super::FunctionExports] = &[
     libc::errno::FUNCTIONS,
     libc::ifaddrs::FUNCTIONS,
     libc::keymgr::FUNCTIONS,
-    libc::mach_semaphore::FUNCTIONS,
     libc::mach_host::FUNCTIONS,
     libc::mach_thread_info::FUNCTIONS,
     libc::mach_time::FUNCTIONS,
