78874eaf89c289b72aae892f23f4de24cd96d863
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-05-07 16:56:02 +0200

    Update ns_process_info.rs

--- src/frameworks/foundation/ns_process_info.rs
+++ src/frameworks/foundation/ns_process_info.rs
@@ -6,14 +6,24 @@
 //! `NSProcessInfo`.
 
 use super::NSTimeInterval;
-use crate::objc::{objc_classes, ClassExports};
+use crate::objc::{id, objc_classes, ClassExports};
 use std::time::Instant;
+use crate::frameworks::foundation::ns_string;
 
 pub const CLASSES: ClassExports = objc_classes! {
 
 (env, this, _cmd);
 
 @implementation NSProcessInfo: NSObject
+
++ (id)processInfo {
+    this
+}
+
+// FIXME: it should be an instance method
++ (id)processName {
+    ns_string::get_static_str(env, "MY PROCESS NAME")
+}
 
 + (NSTimeInterval)systemUptime {
     Instant::now().duration_since(env.startup_time).as_secs_f64()
