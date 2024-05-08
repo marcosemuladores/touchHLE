a5539b2dbe897b010a9d65990b33ffa44a6da85d
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-05-08 20:05:44 +0200

    Update store_kit.rs

--- src/frameworks/store_kit.rs
+++ src/frameworks/store_kit.rs
@@ -4,11 +4,5 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-pub mod sk_payment_queue;
 pub mod sk_product;
 pub mod sk_request;
-
-#[derive(Default)]
-pub struct State {
-    sk_payment_queue: sk_payment_queue::State
-}
