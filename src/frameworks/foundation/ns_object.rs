29fc8ecfae4de984688a4a1589223b7f98452053
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-05-11 18:40:12 +0200

    Update ns_object.rs

--- src/frameworks/foundation/ns_object.rs
+++ src/frameworks/foundation/ns_object.rs
@@ -275,7 +275,7 @@ forUndefinedKey:(id)key { // NSString*
 
     assert!(env.current_thread != 0);
     log!("performSelectorOnMainThread:{} withObject:{:?} waitUntilDone:{}", sel.as_str(&env.mem), arg, wait);
-    assert!(!wait);
+    assert!(wait);
 
     let sel_key: id = ns_string::get_static_str(env, "SEL");
     let sel_str = ns_string::from_rust_string(env, sel.as_str(&env.mem).to_string());
