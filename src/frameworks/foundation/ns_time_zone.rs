da3262884721059b750296c2eac51080893b84d2
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-05-18 08:26:15 +0200

    Update ns_time_zone.rs

--- src/frameworks/foundation/ns_time_zone.rs
+++ src/frameworks/foundation/ns_time_zone.rs
@@ -5,7 +5,15 @@
  */
 //! `NSTimeZone`.
 
-use crate::objc::{id, nil, objc_classes, ClassExports};
+use crate::frameworks::foundation::{ns_string, NSInteger};
+use crate::objc::{autorelease, id, nil, release, retain, ClassExports, HostObject, NSZonePtr};
+use crate::{msg, objc_classes};
+
+struct NSTimeZoneHostObject {
+    // NSString*
+    time_zone: id,
+}
+impl HostObject for NSTimeZoneHostObject {}
 
 pub const CLASSES: ClassExports = objc_classes! {
 
@@ -13,8 +21,41 @@ pub const CLASSES: ClassExports = objc_classes! {
 
 @implementation NSTimeZone: NSObject
 
-+ (id)localTimeZone { // NSTimeZone*
-    nil
++ (id)allocWithZone:(NSZonePtr)_zone {
+    let host_object = Box::new(NSTimeZoneHostObject {
+        time_zone: nil,
+    });
+    env.objc.alloc_object(this, host_object, &mut env.mem)
+}
+
++ (id)timeZoneWithName:(id)tz_name {
+    let new: id = msg![env; this alloc];
+    let new: id = msg![env; new initWithName:tz_name];
+    autorelease(env, new)
+}
+
++ (id)localTimeZone {
+    // As reported by the Aspen Simulator
+    let tz_name: id = ns_string::get_static_str(env, "Canada/Eastern");
+    msg![env; this timeZoneWithName:tz_name]
+}
+
+- (())dealloc {
+    let tz_name = env.objc.borrow_mut::<NSTimeZoneHostObject>(this).time_zone;
+    release(env, tz_name);
+    env.objc.dealloc_object(this, &mut env.mem)
+}
+
+- (id)initWithName:(id)tz_name { // NSString *
+    assert_ne!(tz_name, nil);
+    retain(env, tz_name);
+    env.objc.borrow_mut::<NSTimeZoneHostObject>(this).time_zone = tz_name;
+    this
+}
+
+- (NSInteger)secondsFromGMT {
+    // TODO: respect timezone
+    0
 }
 
 @end
