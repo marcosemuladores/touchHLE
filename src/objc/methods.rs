e2346635aad5bce0fe1b2314cbaab2b76113a455
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-07-02 13:28:23 +0200

    Update methods.rs

--- src/objc/methods.rs
+++ src/objc/methods.rs
@@ -12,7 +12,7 @@ use super::{
     id, nil, objc_super, Class, ClassHostObject, MsgSendSignature, MsgSendSuperSignature, ObjC, SEL,
 };
 use crate::abi::{CallFromGuest, DotDotDot, GuestArg, GuestFunction, GuestRet};
-use crate::mem::{guest_size_of, ConstPtr, GuestUSize, Mem, Ptr, SafeRead};
+use crate::mem::{guest_size_of, ConstPtr, GuestUSize, Mem, MutPtr, MutVoidPtr, Ptr, SafeRead};
 use crate::Environment;
 use std::any::TypeId;
 
@@ -108,6 +108,30 @@ struct method_t {
 }
 unsafe impl SafeRead for method_t {}
 
+/// The layout of a property list in an app binary.
+///
+/// The name, field names and field layout are based on what Ghidra outputs.
+#[repr(C, packed)]
+pub(super) struct ivar_list_t {
+    entsize: GuestUSize,
+    count: GuestUSize,
+    // entries follow the struct
+}
+unsafe impl SafeRead for ivar_list_t {}
+
+/// The layout of a property in an app binary.
+///
+/// The name, field names and field layout are based on what Ghidra outputs.
+#[repr(C, packed)]
+struct ivar_t {
+    offset: ConstPtr<GuestUSize>,
+    name: ConstPtr<u8>,
+    type_: ConstPtr<u8>,
+    alignment: u32,
+    size: u32,
+}
+unsafe impl SafeRead for ivar_t {}
+
 impl ClassHostObject {
     // See classes.rs for host method parsing
 
@@ -137,6 +161,46 @@ impl ClassHostObject {
             // We must deduplicate it like any other.
             let sel = objc.register_bin_selector(name, mem);
             self.methods.insert(sel, IMP::Guest(imp));
+        }
+    }
+
+    pub(super) fn add_ivars_from_bin(
+        &mut self,
+        ivar_list_ptr: ConstPtr<ivar_list_t>,
+        mem: &Mem,
+        objc: &mut ObjC,
+    ) {
+        // Ensure the superclass hasn't "grown into" the subclass.
+        // TODO: Shift offsets if the superclass has "grown into" the subclass
+        // pointers (https://alwaysprocessing.blog/2023/03/12/objc-ivar-abi)
+        if self.superclass != nil {
+            let &ClassHostObject {
+                instance_start: superclass_instance_start,
+                ..
+            } = objc.borrow(self.superclass);
+            assert!(self.instance_start >= superclass_instance_start);
+        }
+
+        let ivar_list_t { entsize, count } = mem.read(ivar_list_ptr);
+        assert!(entsize >= guest_size_of::<ivar_t>());
+
+        let ivars_base_ptr: ConstPtr<ivar_t> = (ivar_list_ptr + 1).cast();
+
+        for i in 0..count {
+            let ivar_ptr: ConstPtr<ivar_t> = Ptr::from_bits(ivars_base_ptr.to_bits() + i * entsize);
+
+            // TODO: support type strings
+            let ivar_t {
+                offset,
+                name,
+                type_: _,
+                // TODO: Use these values when shifting offsets
+                alignment: _,
+                size: _,
+            } = mem.read(ivar_ptr);
+
+            let name_string = mem.cstr_at_utf8(name).unwrap().into();
+            self.ivars.insert(name_string, offset);
         }
     }
 }
@@ -218,5 +282,74 @@ impl ObjC {
                 class = superclass;
             }
         }
+    }
+
+    /// Checks if the provided class has an ivar in it's class chain.
+    pub fn object_lookup_ivar(
+        &self,
+        mem: &Mem,
+        obj: id,
+        name: &String,
+    ) -> Option<MutPtr<GuestUSize>> {
+        let mut class = ObjC::read_isa(obj, mem);
+        loop {
+            let &ClassHostObject {
+                superclass,
+                ref ivars,
+                ..
+            } = self.borrow(class);
+            if let Some(ivar_offset_ptr) = ivars.get(name) {
+                let ivar_offset = mem.read(*ivar_offset_ptr);
+                let ivar_ptr = MutVoidPtr::from_bits(obj.to_bits() + ivar_offset);
+                return Some(ivar_ptr.cast());
+            } else if superclass == nil {
+                return None;
+            } else {
+                class = superclass;
+            }
+        }
+    }
+
+    pub fn debug_all_class_selectors_as_strings(&self, mem: &Mem, class: Class) -> Vec<String> {
+        let mut class = class;
+        let mut selector_strings = Vec::new();
+        loop {
+            let &ClassHostObject {
+                superclass,
+                ref methods,
+                ..
+            } = self.borrow(class);
+            let mut class_selector_strings = methods
+                .keys()
+                .map(|sel| sel.as_str(mem).to_string())
+                .collect();
+            selector_strings.append(&mut class_selector_strings);
+            if superclass == nil {
+                break;
+            } else {
+                class = superclass;
+            }
+        }
+        selector_strings
+    }
+
+    pub fn debug_all_class_ivars_as_strings(&self, class: Class) -> Vec<String> {
+        let mut class = class;
+        let mut selector_strings = Vec::new();
+        loop {
+            let &ClassHostObject {
+                superclass,
+                ref ivars,
+                ..
+            } = self.borrow(class);
+            let mut class_ivars_strings = ivars.keys().cloned().collect();
+            selector_strings.append(&mut class_ivars_strings);
+            if superclass == nil {
+                break;
+            } else {
+                class = superclass;
+            }
+        }
+        selector_strings
     }
 }
