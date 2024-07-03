e2d42e85efa25360158bca5e2c4efb79b52280ae
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-07-03 18:28:16 +0200

    Update ns_string.rs

--- src/frameworks/foundation/ns_string.rs
+++ src/frameworks/foundation/ns_string.rs
@@ -992,6 +992,10 @@ pub const CLASSES: ClassExports = objc_classes! {
     msg_class![env; _touchHLE_NSMutableString allocWithZone:zone]
 }
 
++ (id)stringWithCapacity:(NSUInteger)_capacity {
+    msg_class![env; NSMutableString new]
+}
+    
 // NSCopying implementation
 - (id)copyWithZone:(NSZonePtr)_zone {
     todo!(); // TODO: this should produce an immutable copy
@@ -1009,6 +1013,18 @@ pub const CLASSES: ClassExports = objc_classes! {
     assert_ne!(format, nil);
     let res = with_format(env, format, args.start());
     *env.objc.borrow_mut(this) = StringHostObject::Utf8(format!("{}{}", to_rust_string(env, this), res).into());
+}
+
+- (())setString:(id)aString { // NSString*
+    let str = to_rust_string(env, aString);
+    let host_object = StringHostObject::Utf8(str);
+    *env.objc.borrow_mut(this) = host_object;
+}
+
+- (())appendString:(id)aString { // NSString*
+    // TODO: this is inefficient? append in place instead
+    let new: id = msg![env; this stringByAppendingString:aString];
+    () = msg![env; this setString:new];
 }
 
 - (())setString:(id)a_string { // NSString*
