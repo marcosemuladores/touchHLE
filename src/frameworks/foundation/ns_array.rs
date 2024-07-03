77b39e0e95822ead6e26a5f640b3525906440e86
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-07-03 15:26:06 +0200

    Update ns_array.rs

--- src/frameworks/foundation/ns_array.rs
+++ src/frameworks/foundation/ns_array.rs
@@ -17,6 +17,7 @@ use crate::objc::{
 };
 use crate::Environment;
 use std::cmp::min;
+use std::mem;
 use std::ops::Add;
 
 struct ObjectEnumeratorHostObject {
@@ -237,6 +238,14 @@ pub const CLASSES: ClassExports = objc_classes! {
     msg![env; new initWithArray: this]
 }
 
+ -(())addObjectsFromArray:(id)other {
+    let count: NSUInteger = msg![env; other count];
+    for i in 0..count {
+        let obj: id = msg![env; other objectAtIndex: i];
+        () = msg![env; this addObject: obj];
+    }
+}
+   
 @end
 
 // Our private subclass that is the single implementation of NSArray for the
@@ -376,6 +385,42 @@ pub const CLASSES: ClassExports = objc_classes! {
     release(env, object)
 }
 
+- (())removeObject:(id)needle {
+    let mut objects = mem::take(&mut env.objc.borrow_mut::<ArrayHostObject>(this).array);
+    retain(env, needle);
+    objects.retain(|&obj| {
+        if obj == needle || msg![env; needle isEqual: obj] {
+            release(env, obj);
+            false
+        } else {
+            true
+        }
+    });
+    release(env, needle);
+    env.objc.borrow_mut::<ArrayHostObject>(this).array = objects;
+}
+
+- (())insertObject:(id)obj
+           atIndex:(NSUInteger)index {
+    let obj = retain(env, obj);
+    env.objc.borrow_mut::<ArrayHostObject>(this).array.insert(index as usize, obj);
+}
+
+- (())replaceObjectAtIndex:(NSUInteger)index
+                withObject:(id)obj {
+    let obj = retain(env, obj);
+    let old = env.objc.borrow_mut::<ArrayHostObject>(this).array[index as usize];
+    env.objc.borrow_mut::<ArrayHostObject>(this).array[index as usize] = obj;
+    release(env, old);
+}
+
+- (())removeAllObjects {
+    let objects = mem::take(&mut env.objc.borrow_mut::<ArrayHostObject>(this).array);
+    for object in objects {
+        release(env, object);
+    }
+}
+    
 @end
 
 // Special variant for use by CFArray with NULL callbacks: objects aren't
