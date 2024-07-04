930e0d0add6a755ee41d412e77032d4c90ad5b1d
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-07-04 22:58:37 +0200

    Update main.c

--- tests/TestApp_source/main.c
+++ tests/TestApp_source/main.c
@@ -95,8 +95,33 @@ typedef __pthread_t pthread_t;
 typedef struct opaque_pthread_attr_t opaque_pthread_attr_t;
 typedef struct opaque_pthread_attr_t *__pthread_attr_t;
 typedef __pthread_attr_t pthread_attr_t;
+
+struct _opaque_pthread_mutex_t { long __sig; char __opaque[40]; };
+typedef struct _opaque_pthread_mutex_t __pthread_mutex_t;
+typedef __pthread_mutex_t pthread_mutex_t;
+
+typedef struct opaque_pthread_mutexattr_t opaque_pthread_mutexattr_t;
+typedef struct opaque_pthread_mutexattr_t *__pthread_mutexattr_t;
+typedef __pthread_mutexattr_t pthread_mutexattr_t;
+
+typedef struct opaque_pthread_cond_t opaque_pthread_cond_t;
+typedef struct opaque_pthread_cond_t *__pthread_cond_t;
+typedef __pthread_cond_t pthread_cond_t;
+
+typedef struct opaque_pthread_condattr_t opaque_pthread_condattr_t;
+typedef struct opaque_pthread_condattr_t *__pthread_condattr_t;
+typedef __pthread_condattr_t pthread_condattr_t;
+
 int pthread_create(pthread_t *, const pthread_attr_t *, void *(*)(void *),
                    void *);
+
+int pthread_cond_init(pthread_cond_t *, const pthread_condattr_t *);
+int pthread_cond_signal(pthread_cond_t *);
+int pthread_cond_wait(pthread_cond_t *, pthread_mutex_t *);
+
+int pthread_mutex_init(pthread_mutex_t *, const pthread_mutexattr_t *);
+int pthread_mutex_lock(pthread_mutex_t *);
+int pthread_mutex_unlock(pthread_mutex_t *);
 
 // <semaphore.h>
 #define SEM_FAILED ((sem_t *)-1)
@@ -618,6 +643,42 @@ int test_sem() {
     return -1;
   }
 
+int done = 0;
+pthread_mutex_t m;
+pthread_cond_t c;
+
+void thr_exit() {
+  pthread_mutex_lock(&m);
+  done = 1;
+  pthread_cond_signal(&c);
+  pthread_mutex_unlock(&m);
+}
+
+void *child(void *arg) {
+  thr_exit();
+  return NULL;
+}
+
+void thr_join() {
+  pthread_mutex_lock(&m);
+  while (done == 0) {
+    pthread_cond_wait(&c, &m);
+  }
+  pthread_mutex_unlock(&m);
+}
+
+int test_cond_var() {
+  pthread_t p;
+
+  pthread_mutex_init(&m, NULL);
+  pthread_cond_init(&c, NULL);
+
+  pthread_create(&p, NULL, child, NULL);
+  thr_join();
+
+  return done == 1 ? 0 : -1;
+}
+  
   // Check that reopen is fine
   semaphore = sem_open("sem_test", O_CREAT, 0644, 1);
   if (semaphore == SEM_FAILED) {
@@ -978,6 +1039,7 @@ struct {
     FUNC_DEF(test_strtof),
     FUNC_DEF(test_getcwd_chdir),
     FUNC_DEF(test_sem),
+    FUNC_DEF(test_cond_var),
     FUNC_DEF(test_CGAffineTransform),
     FUNC_DEF(test_strncpy),
     FUNC_DEF(test_strncat),
