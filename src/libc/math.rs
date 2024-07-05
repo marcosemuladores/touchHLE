df4a739d1a8fd6d681931ab95c9e9cd0bc2d0ff1
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-07-05 22:41:07 +0200

    Update math.rs

--- src/libc/math.rs
+++ src/libc/math.rs
@@ -237,6 +237,9 @@ fn socket(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
 fn ioctl(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
     arg1.min(arg2)
 }
+fn listen(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
+    arg1.min(arg2)
+}
 fn hypot(env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
     sqrt(env, arg1*arg1 + arg2*arg2)
 }
@@ -410,6 +413,7 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(setsockopt(_, _)),
     export_c_func!(socket(_, _)),
     export_c_func!(ioctl(_, _)),
+    export_c_func!(listen(_, _)),
     export_c_func!(hypot(_, _)),
     export_c_func!(lrint(_)),
     export_c_func!(lrintf(_)),
