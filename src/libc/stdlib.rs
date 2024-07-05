c08b1777e112e65d303c7cffaea0017114cee805
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-07-05 12:57:37 +0200

    Update stdlib.rs

--- src/libc/stdlib.rs
+++ src/libc/stdlib.rs
@@ -145,6 +145,9 @@ fn arc4random(env: &mut Environment) -> u32 {
 fn getenv(env: &mut Environment, name: ConstPtr<u8>) -> MutPtr<u8> {
     init_env_vars(env);
     let name_cstr = env.mem.cstr_at(name).to_owned();
+    if "XML_MEM_BREAKPOINT".as_bytes() == name_cstr || "XML_MEM_TRACE".as_bytes() == name_cstr {
+        return Ptr::null();
+    }
     // TODO: Provide all the system environment variables an app might expect to
     // find. Currently the only environment variables that can be found are
     // HOME and those set by the app (Used by Crash Bandicoot Nitro Kart 3D)
