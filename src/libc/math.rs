a95a428ef5f7aaed96c6c85254712fa1520e7a17
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-07-06 14:23:38 +0200

    Update math.rs

--- src/libc/math.rs
+++ src/libc/math.rs
@@ -241,6 +241,9 @@ fn gzopen(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
 fn gzread(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
     arg1.min(arg2)
 }
+fn gzclose(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
+    arg1.min(arg2)
+}
 fn setsockopt(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
     arg1.min(arg2)
 }
@@ -433,6 +436,7 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(fminf(_, _)),
     export_c_func!(gzopen(_, _)),
     export_c_func!(gzread(_, _)),
+    export_c_func!(gzclose(_, _)),
     export_c_func!(setsockopt(_, _)),
     export_c_func!(socket(_, _)),
     export_c_func!(ioctl(_, _)),
