52440879af2e5b5bbf6b747e2f7b8a864d54bd90
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-07-07 09:38:46 +0200

    Update posix_io.rs

--- src/libc/posix_io.rs
+++ src/libc/posix_io.rs
@@ -302,7 +302,17 @@ pub fn write(
     buffer: ConstVoidPtr,
     size: GuestUSize,
 ) -> GuestISize {
+    if fd == STDERR_FILENO {
+        let buffer_slice = env.mem.bytes_at(buffer.cast(), size);
+        return match std::io::stderr().write(buffer_slice) {
+            Ok(bytes_written) => bytes_written as GuestUSize,
+            Err(_err) => 0,
+        } as GuestISize
+    }
     // TODO: error handling for unknown fd?
+    // if env.libc_state.posix_io.file_for_fd(fd).is_none() {
+    //     return -1;
+    // }
     let file = env.libc_state.posix_io.file_for_fd(fd).unwrap();
 
     let buffer_slice = env.mem.bytes_at(buffer.cast(), size);
