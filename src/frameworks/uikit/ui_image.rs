98101bfe2f85656a0019fa486123be372f237ddb
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-07-07 13:08:51 +0200

    Update ui_image.rs

--- src/frameworks/uikit/ui_image.rs
+++ src/frameworks/uikit/ui_image.rs
@@ -7,7 +7,7 @@
 
 use crate::frameworks::core_graphics::cg_context::CGContextDrawImage;
 use crate::frameworks::core_graphics::cg_image::{self, CGImageRef, CGImageRelease, CGImageRetain};
-use crate::frameworks::core_graphics::{CGPoint, CGRect, CGSize};
+use crate::frameworks::core_graphics::{CGRect, CGSize};
 use crate::frameworks::foundation::{ns_data, ns_string, NSInteger};
 use crate::frameworks::uikit::ui_graphics::UIGraphicsGetCurrentContext;
 use crate::fs::GuestPath;
@@ -125,12 +125,6 @@ pub const CLASSES: ClassExports = objc_classes! {
         width: width as _,
         height: height as _,
     }
-}
-
-- (())drawInPoint:(CGPoint)point {
-    let context = UIGraphicsGetCurrentContext(env);
-    let image = env.objc.borrow::<UIImageHostObject>(this).cg_image;
-    CGContextDrawImage(env, context, point, image);
 }
 
 - (())drawInRect:(CGRect)rect {
