2be9806520af36268473692c8b65e76dd18addfc
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-07-09 08:14:32 +0200

    Update class_lists.rs

--- src/objc/classes/class_lists.rs
+++ src/objc/classes/class_lists.rs
@@ -7,13 +7,14 @@
 //! very long and frequently-updated list.
 
 use crate::frameworks::{
-    av_audio, core_animation, core_foundation, core_graphics, foundation, media_player, opengles,
+    av_audio, avf_audio, core_animation, core_foundation, core_graphics, foundation, media_player, opengles,
     store_kit, uikit,
 };
 
 /// All the lists of classes that the runtime should search through.
 pub const CLASS_LISTS: &[super::ClassExports] = &[
     crate::app_picker::CLASSES, // Not a framework! Special internal classes.
+    avf_audio::av_audio_session::CLASSES,
     core_animation::ca_eagl_layer::CLASSES,
     core_animation::ca_layer::CLASSES,
     core_graphics::cg_data_provider::CLASSES,
