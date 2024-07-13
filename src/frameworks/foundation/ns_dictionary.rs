e9c1e83aa528dacadf8140c0222709cd978f6a76
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-07-13 07:19:18 +0200

    Update ns_dictionary.rs

--- src/frameworks/foundation/ns_dictionary.rs
+++ src/frameworks/foundation/ns_dictionary.rs
@@ -492,10 +492,6 @@ pub const CLASSES: ClassExports = objc_classes! {
     msg![env; val boolValue]
 }
 
-- (id)stringForKey:(id)defaultName {
-    msg![env; this objectForKey:defaultName]
-}
-
 @end
 
 };
