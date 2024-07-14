dad62ad71591e7ebdb86fc769da30a4ce95df985
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-07-14 12:45:32 +0200

    Update ns_data.rs

--- src/frameworks/foundation/ns_data.rs
+++ src/frameworks/foundation/ns_data.rs
@@ -8,7 +8,7 @@
 use super::ns_string::to_rust_string;
 use super::{NSRange, NSUInteger};
 use crate::fs::GuestPath;
-use crate::frameworks::foundation::ns_value::NSNumberHostObject,
+use crate::frameworks::foundation::ns_value::NSNumberHostObject;
 use crate::mem::{ConstPtr, ConstVoidPtr, MutPtr, MutVoidPtr, Ptr};
 use crate::objc::{
     autorelease, id, msg, nil, objc_classes, release, retain, ClassExports, HostObject, NSZonePtr,
