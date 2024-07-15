dd463d0cf86455ae61d717961438f7b6084ac74d
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-07-15 10:28:38 +0200

    Update ns_data.rs

--- src/frameworks/foundation/ns_data.rs
+++ src/frameworks/foundation/ns_data.rs
@@ -33,7 +33,7 @@ pub const CLASSES: ClassExports = objc_classes! {
 + (id)allocWithZone:(NSZonePtr)_zone {
     let host_object = Box::new(NSDataHostObject {
         bytes: Ptr::null(),
-        freeWhenDone: BOOL,
+        freeWhenDone: u32,
         length: 0,
     });
     env.objc.alloc_object(this, host_object, &mut env.mem)
