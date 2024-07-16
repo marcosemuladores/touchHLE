1bce78b2298392326f69d66107eca35469496138
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-07-16 13:46:25 +0200

    Update fs.rs

--- src/fs.rs
+++ src/fs.rs
@@ -572,15 +572,6 @@ impl Fs {
         &self.home_directory
     }
 
-    pub fn change_current_directory(&mut self, path: &str) {
-        if path.starts_with('/') {
-            self.current_directory = GuestPathBuf::from(GuestPath::new(&path));
-        } else {
-            self.current_directory = self.current_directory.join(path);
-        }
-        assert!(self.exists(&self.current_directory));
-    }
-
     /// Get the absolute path of the current working directory. The resulting
     /// path may be invalid if the directory was moved or deleted.
     pub fn working_directory(&self) -> &GuestPath {
