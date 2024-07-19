4508c362f1ef0a7ee81fffa1f404b16c379a90d6
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-07-19 14:02:50 +0300

    Update math.rs

--- src/libc/math.rs
+++ src/libc/math.rs
@@ -235,6 +235,9 @@ fn fmin(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
 fn fminf(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
     arg1.min(arg2)
 }
+fn glDrawTexiOES(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
+    arg1.min(arg2)
+}
 fn glSampleCoverage(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
     arg1.min(arg2)
 }
@@ -511,6 +514,7 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(fmaxf(_, _)),
     export_c_func!(fmin(_, _)),
     export_c_func!(fminf(_, _)),
+    export_c_func!(glDrawTexiOES(_, _)),
     export_c_func!(glSampleCoverage(_, _)),
     export_c_func!(abort(_, _)),
     export_c_func!(gzopen(_, _)),
