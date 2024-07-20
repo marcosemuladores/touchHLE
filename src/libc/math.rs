cf02400ebe31cf3ad70048d12467d9058d938ee1
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-07-20 03:12:54 +0300

    Update math.rs

--- src/libc/math.rs
+++ src/libc/math.rs
@@ -289,6 +289,9 @@ fn glGetAttribLocation(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
 fn glGetUniformLocation(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
     arg1.min(arg2)
 }
+fn glReadPixels(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
+    arg1.min(arg2)
+}
 fn abort(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
     arg1.min(arg2)
 }
@@ -580,6 +583,7 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(glGetProgramiv(_, _)),
     export_c_func!(glGetAttribLocation(_, _)),
     export_c_func!(glGetUniformLocation(_, _)),
+    export_c_func!(glReadPixels(_, _)),
     export_c_func!(abort(_, _)),
     export_c_func!(gzopen(_, _)),
     export_c_func!(gzread(_, _)),
