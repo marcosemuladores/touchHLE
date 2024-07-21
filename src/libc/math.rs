6773a5d1d70c4908279a38bc1533880ad56d7389
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-07-21 22:28:46 +0300

    Update math.rs

--- src/libc/math.rs
+++ src/libc/math.rs
@@ -247,6 +247,9 @@ fn CGDataProviderCreateDirect(_env: &mut Environment, arg1: f32, arg2: f32) -> f
 fn CGImageCreate(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
     arg1.min(arg2)
 }
+fn CGImageCreateWithJPEGDataProvider(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
+    arg1.min(arg2)
+}
 fn glDrawTexiOES(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
     arg1.min(arg2)
 }
@@ -593,6 +596,7 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(AudioSessionGetPropertySize(_, _)),
     export_c_func!(CGDataProviderCreateDirect(_, _)),
     export_c_func!(CGImageCreate(_, _)),
+    export_c_func!(CGImageCreateWithJPEGDataProvider(_, _)),
     export_c_func!(glDrawTexiOES(_, _)),
     export_c_func!(glBindFramebuffer(_, _)),
     export_c_func!(glCheckFramebufferStatus(_, _)),
