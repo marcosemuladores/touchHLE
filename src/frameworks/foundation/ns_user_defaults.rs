5735c174d801aa861480a0ed674373e8383baa88
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-07-25 19:05:44 +0200

    Update ns_user_defaults.rs

--- src/frameworks/foundation/ns_user_defaults.rs
+++ src/frameworks/foundation/ns_user_defaults.rs
@@ -48,6 +48,13 @@ pub const CLASSES: ClassExports = objc_classes! {
     msg![env; val boolValue]
 }
 
+- (id)objectForKey:(id)key {
+    let host_obj: DictionaryHostObject = std::mem::take(env.objc.borrow_mut(this));
+    let res = host_obj.lookup(env, key);
+    *env.objc.borrow_mut(this) = host_obj;
+    res
+}
+
 // TODO: plist methods etc
 
 @end
