7f6fba75ead6bc0d449f398bb741ec3fb8c87049
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-07-25 08:21:29 +0200

    Update math.rs

--- src/libc/math.rs
+++ src/libc/math.rs
@@ -403,6 +403,9 @@ fn sqlite3_finalize(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
 fn sqlite3_mprintf(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
     arg1.min(arg2)
 }
+fn sqlite3_close(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
+    arg1.min(arg2)
+}
 fn putc(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
     arg1.min(arg2)
 }
@@ -651,6 +654,7 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(sqlite3_step(_, _)),
     export_c_func!(sqlite3_finalize(_, _)),
     export_c_func!(sqlite3_mprintf(_, _)),
+    export_c_func!(sqlite3_close(_, _)),
     export_c_func!(putc(_, _)),
     export_c_func!(getc(_, _)),
     export_c_func!(ungetc(_, _)),
